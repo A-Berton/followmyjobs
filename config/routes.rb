146862dd8912374ca26aafb4da8e4591af6c5c32
A.BERTON <bertonaubry.a@gmail.com>
2021-04-10 19:09:57 +0200

    creating jobs routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'job/list'
+  get 'job/new'
+  post 'job/create'
+  patch 'job/update'
+  get 'job/list'
+  get 'job/show'
+  get 'job/edit'
+  get 'job/delete'
+  get 'job/update'
 end
